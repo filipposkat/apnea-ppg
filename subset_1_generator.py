e98f95d6f5b8c30386c8947ccaf948b1f4cc5e5b
filipposkat <filipposkat@icloud.com>
2023-11-22 00:11:54 +0200

    No events to events ratio can be specified

--- subset_1_generator.py
+++ subset_1_generator.py
@@ -1,3 +1,4 @@
+from itertools import filterfalse, count
 from pathlib import Path
 import numpy as np
 from tqdm import tqdm
@@ -17,9 +18,12 @@ SUBSET_SIZE = 400  # The number of subjects that will remain after screening dow
 WINDOW_SEC_SIZE = 16
 SIGNALS_FREQUENCY = 32  # The frequency used in the exported signals
 STEP = 2  # The step between each window
-CREATE_ARRAYS = True
-SEED = 33
 TEST_SIZE = 0.2
+NO_EVENTS_TO_EVENTS_RATIO = 10
+CREATE_ARRAYS = True
+SKIP_EXISTING_IDS = True
+SEED = 33
+
 WINDOW_SAMPLES_SIZE = WINDOW_SEC_SIZE * SIGNALS_FREQUENCY
 
 with open("config.yml", 'r') as f:
@@ -30,7 +34,7 @@ if config is not None:
     PATH_TO_SUBSET1 = config["paths"]["local"]["subset_1_directory"]
 else:
     PATH_TO_OBJECTS = Path(__file__).joinpath("data", "serialized-objects")
-    PATH_TO_SUBSET1 =Path(__file__).joinpath("data", "subset-1")
+    PATH_TO_SUBSET1 = Path(__file__).joinpath("data", "subset-1")
 # --- END OF CONSTANTS --- #
 
 path = Path(PATH_TO_SUBSET1).joinpath("ids.npy")
@@ -103,67 +107,81 @@ else:
 print(f"Final subset size: {len(best_ids)}")
 print(best_ids)
 
+
+def assign_window_label(window_labels: pd.Series) -> int:
+    """
+    :param window_labels: A pandas Series with the event labels for each sample in the window
+    :return: One label representing the whole window, specifically the label with most occurrences.
+    """
+    if window_labels.max() == 0:
+        return 0
+    else:
+        # 0=no events, 1=central apnea, 2=obstructive apnea, 3=hypopnea, 4=spO2 desaturation, 5=other event
+        event_counts = window_labels.value_counts()  # Series containing counts of unique values.
+        prominent_event_index = event_counts.argmax()
+        prominent_event = event_counts.index[prominent_event_index]
+        # print(event_counts)
+        return int(prominent_event)
+
+
+def get_subject_train_test_data(subject: Subject):
+    sub_df = subject.export_to_dataframe(signal_labels=["Flow", "Pleth"], print_downsampling_details=False)
+    sub_df.drop(["time_secs"], axis=1, inplace=True)
+
+    # Take equal-sized windows with a specified step:
+
+    # 1. Calculate the number of windows:
+    num_windows = (len(sub_df) - WINDOW_SAMPLES_SIZE) // STEP + 1  # a//b = math.floor(a/b)
+    # Note that due to floor division the last WINDOW_SAMPLES_SIZE-1 samples might be dropped
+
+    # 2. Generate equal-sized windows:
+    windows_dfs = [sub_df.iloc[i * STEP:i * STEP + WINDOW_SAMPLES_SIZE] for i in range(num_windows)]
+    X = [window_df.loc[:, window_df.columns != "event_index"] for window_df in windows_dfs]
+    y = [assign_window_label(window_df["event_index"]) for window_df in windows_dfs]
+
+    # 3. Drop no-event windows to archive desired ratio of no-events to events:
+    num_of_event_windows = np.count_nonzero(y)
+    num_of_no_event_windows = len(y) - num_of_event_windows
+    target_num_no_event_windows = NO_EVENTS_TO_EVENTS_RATIO * num_of_event_windows
+    diff = num_of_no_event_windows - target_num_no_event_windows
+
+    # Shuffle X, y without losing order:
+    combined_xy = list(zip(X, y))  # Combine X and y into one list
+    random.shuffle(combined_xy)  # Shuffle
+
+    if diff > 0:
+        # Reduce no event windows by diff:
+        combined_xy = list(filterfalse(lambda Xy, c=count(): Xy[1] == 0 and next(c) < diff, combined_xy))
+    elif diff < 0:
+        # Reduce event windows by diff:
+        combined_xy = list(filterfalse(lambda Xy, c=count(): Xy[1] != 0 and next(c) < diff, combined_xy))
+    X, y = zip(*combined_xy)  # separate Xy again
+
+    # 4. Split into train and test:
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, shuffle=True, stratify=y,
+                                                        random_state=SEED)
+    # print(f"Events train%: {np.count_nonzero(y_train) / len(y_train)}")
+    # print(f"Events test%: {np.count_nonzero(y_test) / len(y_test)}")
+    return X_train, X_test, y_train, y_test
+
+
 if CREATE_ARRAYS:
     random.seed(SEED)  # Set the seed
 
-
-    def assign_window_label(window_labels: pd.Series) -> int:
-        """
-        :param window_labels: A pandas Series with the event labels for each sample in the window
-        :return: One label representing the whole window, specifically the label with most occurrences.
-        """
-        if window_labels.max() == 0:
-            return 0
-        else:
-            # 0=no events, 1=central apnea, 2=obstructive apnea, 3=hypopnea, 4=spO2 desaturation, 5=other event
-            event_counts = window_labels.value_counts()  # Series containing counts of unique values.
-            prominent_event_index = event_counts.argmax()
-            prominent_event = event_counts.index[prominent_event_index]
-            # print(event_counts)
-            return int(prominent_event)
-
-
-    # from random import randrange
-    # test = pd.Series([randrange(5) for i in range(512)])
-    # print(assign_window_label(test))
-
-    # def train_test_split(X, y: list[int]):
-    #     num_of_events = np.count_nonzero(y)
-    #
-    #     # Shuffle X, y at once with same order:
-    #     c = list(zip(X, y))
-    #     random.shuffle(c)
-    #     X_s, y_s = zip(*c)
-
     for (id, sub) in get_subjects_by_ids_generator(best_ids, progress_bar=True):
-        sub_df = sub.export_to_dataframe(signal_labels=["Flow", "Pleth"], print_downsampling_details=False)
-        sub_df.drop(["time_secs"], axis=1, inplace=True)
+        subject_arrs_path = Path(PATH_TO_SUBSET1).joinpath("arrays", str(id).zfill(4))
+        if subject_arrs_path.exists() and SKIP_EXISTING_IDS:
+            continue
 
-        # Take equal-sized windows with a specified step:
+        X_train, X_test, y_train, y_test = get_subject_train_test_data(sub)
 
-        # 1. Calculate the number of windows:
-        num_windows = (len(sub_df) - WINDOW_SAMPLES_SIZE) // STEP + 1  # a//b = math.floor(a/b)
-        # Note that due to floor division the last WINDOW_SAMPLES_SIZE-1 samples might be dropped
-
-        # 2. Generate equal-sized windows:
-        windows_dfs = [sub_df.iloc[i * STEP:i * STEP + WINDOW_SAMPLES_SIZE] for i in range(num_windows)]
-        X = [window_df.loc[:, window_df.columns != "event_index"] for window_df in windows_dfs]
-        y = [assign_window_label(window_df["event_index"]) for window_df in windows_dfs]
-
-        # 3. Split into train and test:
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, shuffle=True, stratify=y,
-                                                            random_state=SEED)
-        # print(f"Events train: {np.count_nonzero(y_train)}")
-        # print(f"Events test: {np.count_nonzero(y_test)}")
-
-        # 4. Save windows as a numpy array file:
+        # Transform to numpy arrays:
         X_train_arr = np.array(X_train)  # shape= (num of windows in train, WINDOW_SAMPLES_SIZE, numOfSignals)
         y_train_arr = np.array(y_train)  # shape= (num of windows in train, WINDOW_SAMPLES_SIZE, 1)
-        X_test_arr = np.array(X_test)    # shape= (num of windows in test, WINDOW_SAMPLES_SIZE, numOfSignals+1)
-        y_test_arr = np.array(y_test)    # shape= (num of windows in test, WINDOW_SAMPLES_SIZE, 1)
+        X_test_arr = np.array(X_test)  # shape= (num of windows in test, WINDOW_SAMPLES_SIZE, numOfSignals+1)
+        y_test_arr = np.array(y_test)  # shape= (num of windows in test, WINDOW_SAMPLES_SIZE, 1)
 
         # Create directory for subject:
-        subject_arrs_path = Path(PATH_TO_SUBSET1).joinpath("arrays", str(id).zfill(4))
         subject_arrs_path.mkdir(parents=True, exist_ok=True)
 
         X_train_path = subject_arrs_path.joinpath("X_train")
@@ -171,11 +189,8 @@ if CREATE_ARRAYS:
         X_test_path = subject_arrs_path.joinpath("X_test")
         y_test_path = subject_arrs_path.joinpath("y_test")
 
+        # Save the arrays
         np.save(X_train_path, X_train_arr)
         np.save(y_train_path, y_train_arr)
         np.save(X_test_path, X_test_arr)
         np.save(y_test_path, y_test_arr)
-
-        # # Display the windows
-        # for i, window in enumerate(windows_dfs):
-        #     print(f"Window {i + 1}:\n{window}\n")
