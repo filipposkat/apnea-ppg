bf923f944f0f91ba84f725474f2f5287d81adf56
filipposkat <filipposkat@icloud.com>
2025-09-27 23:49:45 +0300

    Bug fix

--- tester.py
+++ tester.py
@@ -929,8 +929,8 @@ def test_loop(model: nn.Module, test_dataloader: DataLoader, n_class=5, device="
         apr_by_class[class_name] = apr
 
         pr_info_by_class[class_name] = {"thresholds": thresholds.tolist(),
-                                        "average_precision": list(prec),
-                                        "average_recall": list(rec),
+                                        "average_precision": prec.tolist(),
+                                        "average_recall": rec.tolist(),
                                         "apr": float(apr)}
 
     if CALCULATE_ROC_FOR_MERGED_CLASSES and len(classes) >= 4:
